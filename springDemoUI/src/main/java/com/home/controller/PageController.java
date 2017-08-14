5286ce42b3c53574824ee274311cd5cdc48ffa66
Deepak Shekhawat <deep.shekhawat16@gmail.com>
2017-08-14 23:19:54 +0530

    changes done for project configration

--- springDemoUI/src/main/java/com/home/controller/PageController.java
+++ springDemoUI/src/main/java/com/home/controller/PageController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
+@RequestMapping(value ={"/", "/index", "/home"})
 public class PageController {
 
 	@RequestMapping(value ={"/", "/index", "/home"})
 	public ModelAndView index(){
 		
-		ModelAndView mv = new ModelAndView("page");
+		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("greeting", "Welcome to Spring web MVC..");
 		return mv;
 	}
