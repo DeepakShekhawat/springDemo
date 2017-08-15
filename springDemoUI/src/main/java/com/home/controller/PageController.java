b70c7c4b91e25b9469c6c4b90cb393b92d8d2840
Deepak Shekhawat <deep.shekhawat16@gmail.com>
2017-08-15 13:37:34 +0530

    templated created successfully with loaded all static resources.

--- springDemoUI/src/main/java/com/home/controller/PageController.java
+++ springDemoUI/src/main/java/com/home/controller/PageController.java
@@ -1,16 +1,29 @@
 package com.home.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value ="/home")
+	@RequestMapping("/index")
 	public ModelAndView index(){
 		
 		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("greeting", "Welcome to Spring web MVC..");
 		return mv;
+	}
+		
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test(@PathVariable(value = "greeting", required=false)String greeting){
+		
+		if(greeting==null){
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greeting", greeting);
+		return mv;
+		
 	}
 }
